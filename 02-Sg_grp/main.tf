0121432b0da8262b5c8d5c21adf7ec07d3b608a1
srinath sriramula <sriramulasrinath59@gmail.com>
2024-06-12 13:31:05 +0530

    commiting changes

--- 02-Sg_grp/main.tf
+++ 02-Sg_grp/main.tf
@@ -226,3 +226,22 @@ resource "aws_security_group_rule" "bastion_public" {
   cidr_blocks =  ["0.0.0.0/0"]#where you are getting traffic from
   security_group_id = module.bastion.sg_id
 }
+###############################################################
+###       FOR CICD TOOLS
+#################################################################
+resource "aws_security_group_rule" "backend_default_vpc" {
+  type              = "ingress"
+  from_port         = 22
+  to_port           = 22
+  protocol          = "tcp"
+  cidr_blocks =  ["172.31.0.0/16"]#where you are getting traffic from
+  security_group_id = module.backend.sg_id
+}
+resource "aws_security_group_rule" "frontend_default_vpc" {
+  type              = "ingress"
+  from_port         = 22
+  to_port           = 22
+  protocol          = "tcp"
+  cidr_blocks =  ["172.31.0.0/16"]#where you are getting traffic from
+  security_group_id = module.frontend.sg_id
+}
